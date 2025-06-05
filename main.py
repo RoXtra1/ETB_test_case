23e4237239ac7358067c551b845b1fc147b4b716
RoXtra <skornyakovartem@mail.ru>
2025-06-05 13:48:21 +0200

    split main transaction
    split main transaction on sub-transactions for each manipulation

--- main.py
+++ main.py
@@ -118,7 +118,6 @@ def provodka(session):
             to_account=acc_to.account_number,
             amount=amount)
         session.add(transaction)
-        session.commit()
 
         print(f"\nПеревод выполнен успешно!")
         print(f"Со счета {acc_from.account_number} списано: {amount} руб.")
@@ -128,9 +127,8 @@ def provodka(session):
         print(f"- {acc_to.account_number}: {acc_to.balance} руб.")
 
     except Exception as e:
-        print(f"Ошибка при переводе: {e}")
-        session.rollback()
-
+        print(f"Ошибка при выполнении проводки: {e}")
+        raise
 
 def export_to_xml(session, filename="clients_export.xml"):
     try:
@@ -161,38 +159,34 @@ def export_to_xml(session, filename="clients_export.xml"):
         print(f"Ошибка экспорта: {e}")
         return False
 
-
 def import_from_xml(session, filename="import.xml"):
     try:
         tree = ET.parse(filename)
         root = tree.getroot()
 
-        with session.begin():
-            for client_elem in root.findall('Client'):
-                client = Client(
-                    id=int(client_elem.find('ID').text),
-                    name=client_elem.find('Name').text
-                )
-                session.add(client)
-                session.flush()
+        for client_elem in root.findall('Client'):
+            client = Client(
+                id=int(client_elem.find('ID').text),
+                name=client_elem.find('Name').text
+            )
+            session.add(client)  # добавление данных о клиентах в бд
+            session.flush()  # временное применение внесенных изменений для использования их в последующих изменениях
 
-                accounts_elem = client_elem.find('Accounts')
-                for account_elem in accounts_elem.findall('Account'):
-                    account = Account(
-                        account_number=account_elem.find('Number').text,
-                        balance=Decimal(account_elem.find('Balance').text),
-                        client_id=client.id
-                    )
-                    session.add(account)
+            accounts_elem = client_elem.find('Accounts')
+            for account_elem in accounts_elem.findall('Account'):
+                account = Account(
+                    account_number=account_elem.find('Number').text,
+                    balance=Decimal(account_elem.find('Balance').text),
+                    client_id=client.id
+                )
+                session.add(account)  # добавление данных об аккаунтах клиента в бд
 
         print(f"Данные успешно импортированы из {filename}")
         return True
 
     except Exception as e:
-        session.rollback()
-        print(f"Ошибка импорта: {e}")
-        return False
-
+        print(f"Ошибка при импорте: {e}")
+        raise
 
 def transaction_history(session):
     print("\nИстория операций:")
@@ -224,41 +218,49 @@ def transaction_history(session):
     except Exception as e:
         print(f"Ошибка при получении истории: {e}")
 
+
 if __name__ == "__main__":
     create_tables()  # создание пустых табличек
 
-    Session = sessionmaker(bind=engine)
-    with Session() as session:  # для обеспечения корректного закрытия сессии
-        if session.query(Client).first() is None:  # таблицы заполняются если отсутствуют клиенты
-            try:
-                # fill_tables(session)  функция для заполнения начальными данными не из xml
-                session.commit()
-            except Exception as e:
-                session.rollback()
-                print(f"Ошибка при заполнении таблиц: {e}")
+    Session = sessionmaker(bind=engine)  # создание экземпляра сессии
+
+    # if session.query(Client).first() is None:  # таблицы заполняются если отсутствуют клиенты
+    #     try:
+    #         # fill_tables(session)  функция для заполнения начальными данными не из xml
+    #         session.commit()
+    #     except Exception as e:
+    #         session.rollback()
+    #         print(f"Ошибка при заполнении таблиц: {e}")
+    # else:
+    #     print("В таблице содержатся записи")
+
+    while True:
+        menu()
+        event = input("\nВыберите действие: ")
+
+        if event == "1":
+            with Session(bind=engine.execution_options(readonly=True)) as read_session:  # открытие транзакции исключительно для чтения данных
+                show_clients_list(read_session)
+        elif event == "2":
+            with Session() as session:  # такая же автоматическая точка входа в транзакцию
+                with session.begin():  # точка входа в транзакцию для реализации проводки. автоматически закрывается и автоматически делает откат в случае сбоя
+                    provodka(session)
+        elif event == "3":
+            with Session(bind=engine.execution_options(readonly=True)) as read_session:
+                export_to_xml(read_session)
+        elif event == "4":
+            name = input("Ведите название файла (по дефолту - import.xml)").strip()
+            with Session() as session:
+                with session.begin():
+                    if name:
+                        import_from_xml(session, name)
+                    else:
+                        import_from_xml(session)
+        elif event == "5":
+            with Session(bind=engine.execution_options(readonly=True)) as read_session:
+                transaction_history(read_session)
+        elif event == "0":
+            print("\nВыход из программы...")
+            break
         else:
-            print("В таблице содержатся записи")
-
-        while True:
-            menu()
-            event = input("\nВыберите действие: ")
-
-            if event == "1":
-                show_clients_list(session)
-            elif event == "2":
-                provodka(session)
-            elif event == "3":
-                export_to_xml(session)
-            elif event == "4":
-                name = input("Ведите название файла (по дефолту - import.xml)").strip()
-                if name:
-                    import_from_xml(session, name)
-                else:
-                    import_from_xml(session)
-            elif event == "5":
-                transaction_history(session)
-            elif event == "0":
-                print("\nВыход из программы...")
-                break
-            else:
-                print("Некорректный ввод, попробуйте снова")
+            print("Некорректный ввод, попробуйте снова")
